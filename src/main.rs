cd5d58d299a1ba8178d5f90bff11426306998967
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-09-26 15:38:10 -0600

    Implement basic I/O functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::{
+    mem::{size_of, transmute},
+    thread::sleep,
+    time::Duration,
+};
+
+use winapi::ctypes::c_int;
+use winapi::um::winuser::*;
+
+fn is_mouse_clicked() -> bool {
+    let res = unsafe { GetKeyState(VK_LBUTTON) };
+
+    res & (1 << 15) != 0
+}
+
+fn unclick_mouse() {
+    let mut input = INPUT {
+        type_: INPUT_MOUSE,
+        u: unsafe {
+            transmute(MOUSEINPUT {
+                dx: 0,
+                dy: 0,
+                mouseData: 0,
+                dwFlags: MOUSEEVENTF_LEFTUP,
+                dwExtraInfo: 0,
+                time: 0,
+            })
+        },
+    };
+
+    unsafe { SendInput(1, &mut input as LPINPUT, size_of::<INPUT>() as c_int) };
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mouse_down = is_mouse_clicked();
+        println!(
+            "The mouse is {}",
+            if mouse_down { "down" } else { "not down" }
+        );
+
+        if mouse_down {
+            unclick_mouse();
+        }
+
+        sleep(Duration::from_millis(100));
+    }
 }
